d25b9bbf7b0442dc7581c8b8986c49a5698b438a
Nishant Mendiratta <nishant.mendiratta89@gmail.com>
2017-08-06 23:10:49 -0700

    Initial Commit

---
+++ Tip-CalciTests/Tip_CalciTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_CalciTests.swift
+//  Tip-CalciTests
+//
+//  Created by Nishant Mendiratta on 8/6/17.
+//  Copyright © 2017 Nishant Mendiratta. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_Calci
+
+class Tip_CalciTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
